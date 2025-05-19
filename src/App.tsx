89268e6b691e568c072df7fd8c833e58fbb9bc8f
DiegoBottegoni <diego.bottegoni@gmail.com>
2025-05-19 19:26:29 -0300

    toast from

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import ContactPage from "./presentation/pages/Contact/ContactPage";
+import { Toaster } from 'sonner';
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<ContactPage />} />
-    </Routes>
+    <>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </>
   );
 }
 export default App
