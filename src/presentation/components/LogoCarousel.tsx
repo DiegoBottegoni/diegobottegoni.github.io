fab8a7980dda669c938bc6bdf26a8761cf7a3e44
DiegoBottegoni <diego.bottegoni@gmail.com>
2025-05-20 15:12:16 -0300

    logoCarousel speed up on mobile

--- src/presentation/components/LogoCarousel.tsx
+++ src/presentation/components/LogoCarousel.tsx
@@ -38,7 +38,7 @@ const LogoCarousel = () => {
         let speed = 0.5; // Desktop por defecto
 
         if (screenWidth < 768) {
-            speed = 1.5; // Mobile
+            speed = 2; // Mobile
         } else if (screenWidth < 1024) {
             speed = 2; // Tablet
         }
