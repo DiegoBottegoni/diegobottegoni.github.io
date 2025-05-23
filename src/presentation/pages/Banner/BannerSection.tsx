3cd4ef2552e1fd4748687d77a482034a9e005d12
DiegoBottegoni <diego.bottegoni@gmail.com>
2025-05-23 14:47:49 -0300

    banner spaces fixed

--- src/presentation/pages/Banner/BannerSection.tsx
+++ src/presentation/pages/Banner/BannerSection.tsx
@@ -5,7 +5,7 @@ export default function BannerSection() {
         <div className="flex flex-col bg-primary-d py-24">
             {/* Cinta de arriba: negra con letras blancas */}
             <BannerTape
-                text="FRONTEND DEVELOPER &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; SUPER NERD &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; FRONTEND DEVELOPER &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; SUPER NERD &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; "
+                text="FRONTEND DEVELOPER &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; SUPER NERD &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; FRONTEND DEVELOPER &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; SUPER NERD &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;"
                 backgroundColor="#000000"
                 textColor="#FFFFFF"
                 angle={-3}
